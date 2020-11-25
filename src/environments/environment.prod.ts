3f274a4432d8b0342fdfa85a0d0b4941ab7d3b2f
Sander van Hooff <s.vanhooff@hotmail.com>
2020-11-25 15:17:52 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseAPIKey: 'AIzaSyC2p-KuBM-BJ2DfWNf7YhzCKLXQuvmPKrM', // the unique key used to connect to Google firebase
+  localStorageKey: 'userData' // the key name used to store the user in localStorage
 };
